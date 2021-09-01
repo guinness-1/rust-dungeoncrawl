4bfa9614c9f2127dad53e8a859460df8dabef823
Jamie Floyd <benjaminfloyd@gmail.com>
2021-09-01 10:28:59 -0400

    monsters have tooltips!

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,11 +1,14 @@
 use crate::prelude::*;
 
 mod collision;
+mod end_turn;
 mod entity_render;
+mod hud;
 mod map_render;
+mod movement;
 mod player_input;
 mod random_move;
-mod end_turn;
+mod tooltips;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -13,16 +16,22 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collision::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
@@ -30,10 +39,14 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collision::collisions_system())
-        .flush()    
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,23 +3,23 @@ use crate::prelude::*;
 #[system]
 #[read_component(MovingRandomly)]
 #[write_component(Point)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <&mut Point>::query()
-        .filter(component::<MovingRandomly>());
-    movers
-        .iter_mut(ecs)
-        .for_each(|pos| {
-            let mut rng = RandomNumberGenerator::new();
-            let destination = match rng.range(0, 4) {
-                0 => Point::new(-1, 0),
-                1 => Point::new(1, 0),
-                2 => Point::new(0, -1),
-                _ => Point::new(0, 1),
-            } + *pos;
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point)>::query().filter(component::<MovingRandomly>());
+    movers.iter(ecs).for_each(|(entity, pos)| {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range(0, 4) {
+            0 => Point::new(-1, 0),
+            1 => Point::new(1, 0),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
+        } + *pos;
 
-            if map.can_enter_tile(destination) {
-                *pos = destination;
-            }
-        }
-    );
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
+    });
 }
